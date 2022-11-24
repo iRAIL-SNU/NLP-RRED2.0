84e68f1e7d31865e7e1ae701bc2e7945120dd396
reonaledo <reonaledo@gmail.com>
2022-11-24 08:34:12 +0000

    unfreeze_image_model()

--- source/downstream/utils/utils.py
+++ source/downstream/utils/utils.py
@@ -683,3 +683,13 @@ def post_process(answer):
     elif answer == 'axial plane':
         answer = 'axial'
     return answer
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
